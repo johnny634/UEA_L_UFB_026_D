fe312e5c420477a6cee5e5e6654138031407be22
Jguea24 <jg.grefac@uea.edu.ec>
2024-02-14 13:50:17 -0500

    Se agrega el proyecto.

---
+++ variables_array.py
@@ -0,0 +1,155 @@
+# Declare 6 variables unidimensionales y asigne valores enteros
+
+va_uni_en1 = [1,  2,  3,  4,  5,  6]
+va_uni_en2 = [7,  8,  9,  10, 11, 12]
+va_uni_en3 = [13, 14, 15, 16, 17, 18]
+va_uni_en4 = [19, 20, 21, 22, 23, 24]
+va_uni_en5 = [25, 26, 27, 28, 29, 30]
+va_uni_en6 = [31, 32, 33, 34, 35, 36]
+
+# Declare 6 variables unidimensionales y asigne valores flotantes
+va_uni_flot1 = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6]
+va_uni_flot2 = [2.1, 2.2, 2.3, 2.4, 2.5, 2.6]
+va_uni_flot3 = [3.1, 3.2, 3.3, 3.4, 3.5, 3.6]
+va_uni_flot4 = [4.1, 4.2, 4.3, 4.4, 4.5, 4.6]
+va_uni_flot5 = [5.1, 5.2, 5.3, 5.4, 5.5, 5.6]
+va_uni_flot6 = [6.1, 6.2, 6.3, 6.4, 6.5, 6.6]
+
+# Declare 6 variables unidimensionales y asignar valores de texto
+
+va_uni_tex1 = ['Hola',   'Johnny',  'Grefa', 'Pan',     'Carne',  'Pollo']
+va_uni_tex2 = ['Uno',    'Dos',     'Tres',  'Cuatro',  'Cinco',  'Seis']
+va_uni_tex3 = ['Pedro',  'Casa',    'Carta', 'Bus',     'Ec',     'Pollo']
+va_uni_tex4 = ['Juan',   'Carro',   'Cafe',  'tren',    'Ana',    'Taxi']
+va_uni_tex5 = ['Coca',   'Perro',   'Pez',   'Celular', 'Napo',   'Moto']
+va_uni_tex6 = ['Tena',   'Pera',    'Tigre', 'Pez',     'Texto',  'Parlante']
+
+# Declare 6 variables unidimensionales y asignar valores de enteros, flotantes y texto
+var_ent_flo_tex1 = [1, 1.1, 'Carro']
+var_ent_flo_tex2 = [1, 1.2, 'Perro']
+var_ent_flo_tex3 = [1, 1.3, 'Casa']
+var_ent_flo_tex4 = [1, 1.4, 'Tres']
+var_ent_flo_tex5 = [1, 1.5, 'Tren']
+var_ent_flo_tex6 = [1, 1.6, 'Pez']
+
+#Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asigne valores enteros (6 variables en total)
+va_bi_ent1 = [[1,  2,  3],  [4,  6,  8]]
+va_bi_ent2 = [[10, 22, 33], [12, 16, 17]]
+print(va_bi_ent1)
+# 5x5
+va_bi_ent3 = [[10, 22, 33, 34, 56, 67], [12, 16, 17, 45, 43, 13], [14, 45, 71, 89, 67, 44], [56, 78, 90, 67, 34, 23], [99, 56, 67, 65, 23, 73]]
+va_bi_ent4 = [[11, 21, 31, 41, 51, 61], [78, 45, 18, 33, 66, 45], [15, 67, 54, 67, 43, 77], [14, 45, 71, 89, 67, 44], [14, 45, 71, 89, 67, 44]]
+
+print(va_bi_ent3)
+
+#  3x6
+va_bi_ent5 = [[10, 22, 33, 56, 34, 67], [12, 16, 17, 88, 65, 34], [12, 16, 17, 24, 56, 36]]
+va_bi_ent6 = [[10, 22, 33, 34, 43, 89], [12, 16, 17, 23, 33, 67], [12, 16, 17, 38, 59, 67]]
+
+# Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asigne valores flotantes (6 variables en total)
+
+va_bi_flot1 = [[2.1, 2.2, 2.3], [2.4, 2.5, 2.6]]
+va_bi_flot2 = [[2.1, 2.2, 2.3], [2.4, 2.5, 2.6]]
+
+# variables bidimensionales  5x5
+va_bi_flot3 = [[2.1, 2.2, 2.3, 2.4, 2.5], [2.6, 2.7, 2.8, 2.9, 2.10], [1.1, 1.2, 2.3, 2.4, 2.5], [3.1, 3.2, 3.3, 3.4, 3.5], [4.1, 4.2, 4.3, 4.4, 4.5]]
+va_bi_flot4 = [[5.1, 5.2, 5.3, 5.4, 5.5], [6.6, 6.7, 6.8, 6.9, 6.10], [7.1, 7.2, 7.3, 7.4, 7.5], [8.1, 9.2, 9.3, 9.4, 9.5], [9.1, 9.2, 9.3, 9.4, 9.5]]
+
+# variables bidimensionales  3x6
+va_bi_flot5 = [[12.1, 12.2, 12.3, 12.4, 12.5, 13.6], [13.1, 13.2, 13.3, 13.4, 13.5, 14.6], [14.1, 14.2, 14.3, 14.4, 14.5, 17.6]]
+va_bi_flot6 = [[11.1, 11.2, 11.3, 11.4, 11.5, 11.6], [10.1, 10.2, 10.3, 10.4, 10.5, 10.6], [19.1, 19.2, 18.3, 18.4, 18.5, 18.6]]
+
+#   Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asignar valores de texto (6 variables en total)
+#variables bidimensionales 2x3
+va_bi_tex1 = [['Gato', 'Perez', 'texto'], ['Carro', 'Casa', 'carpa']]
+va_bi_tex2 = [['Ana',  'rio',   'tele'],  ['Ropa',  'Celu', 'trece']]
+
+#variables bidimensionales 5x5
+va_bi_tex3 = [['A',  'B',   'C', 'D', 'E'],  ['F',  'G', 'H', 'I', 'J'], ['K', 'L', 'Ll', 'M', 'N'], ['Ñ', 'O', 'P', 'Q', 'R'], ['S', 'T', 'U', 'V', 'W', 'x']]
+va_bi_tex4 = [['Pez',  'Casa',  'tren', 'rol', 'tres'],  ['recta',  'angular', 'java', 'php', 'rubi'], ['mama', 'hola', 'mundo', 'cuatro', 'cuarto'], ['pais', 'paro', 'pasa', 'yuca', 'canta'], ['carpa', 'trina', 'tres', 'Vega','Wilson','polo']]
+
+#variables bidimensionales 3x6
+va_bi_tex5 = [['texto', 'Carro',  'Casa',   'trece', 'rol', 'recta'],  ['texto', 'Carro', 'Casa', 'trece', 'rol', 'recta'],        ['texto', 'Carro',  'Casa',  'trece',  'rol',  'recta']]
+va_bi_tex6 = [['Carne', 'Carbon', 'Planta', 'admin', 'rol', 'arrepa'], ['Espejo', 'Ren', 'Cuarto', 'cero', 'rolex', 'rectangulo'], ['Cuarto', 'Carmen', 'Jose', 'tebol', 'rock', 'cumbia']]
+
+# Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asignar valores de enteros, flotantes, y texto (6 variables en total)
+
+#2 variables bidimensionales 2x3.
+va_bi_en_flo_tex1 = [[1, 2.3, 'Grefa']]
+va_bi_en_flo_tex2 = [[2, 3.3, 'Cala']]
+
+# 2 variables bidimensionales 5x5.
+va_bi_en_flo_tex3 = [[1, 3.4,  'Grefa', 2, 7.8], [4, 7.5, 'Gre', 5, 8.8],   [8, 6.5, 'Tena', 5, 7.8], [89, 8.5, 'Quito', 3, 7.8], [33, 6.5, 'Carchi', 5, 2.8]]
+va_bi_en_flo_tex4 = [[3, 3, 5, 'Grecia', 3, 4.8], [5, 7.7, 'Misa', 7, 7.8], [6, 3.5, 'Quito', 6, 8.8], [8, 8.5, 'Quinto', 6, 6.8], [36, 8.5, 'Tena', 6, 8.8]]
+
+# 2 variables bidimensionales 3x6
+va_bi_en_flo_tex5 = [[2, 3.1, 'tengo', 3, 5.7, 'Tango'], [1, 3.4, 'tambien', 4, 7.8, 'Quito'], [2, 3.2, 'cantar', 5, 2.5, 'placa']]
+va_bi_en_flo_tex6 = [[2, 3.3, 'Canva', 5, 6.7, 'Carma'], [4, 7.4, 'Cantar',  8, 8.8, 'Quinto'], [6, 7.2, 'www',   7, 7.5, 'robo']]
+
+# Declare 2 variables tridimensionales 2x3x2, 5x5x5, 3x6x2 y asignar valores de enteros.
+
+# variables tridimensionales 2x3x2
+va_tri_en1 = [[[1, 2], [5, 7], [2, 5], [6, 8], [8, 9], [75, 10]]]
+va_tri_en2 = [[[1, 2], [5, 7], [2, 5], [6, 8], [8, 9], [75, 10]]]
+
+# variables tridimensionales 5x5x5
+va_tri_en3 = [[[1,  2,  3,  4,  5],  [11, 12, 13, 14, 15], [21, 22, 23, 24, 25], [31, 32, 33, 44, 35], [41, 42, 43, 44, 45]]]
+va_tri_en4 = [[[41, 42, 43, 44, 45], [1,  21, 33, 64, 75], [61, 82, 93, 94, 65], [61, 52, 73, 74, 85], [81, 52, 63, 46, 85]]]
+
+# variables tridimensionales 3x6x2
+va_tri_en5 = [[[2, 3], [4, 5], [6, 7], [9, 10], [24, 56], [23, 45]]]
+va_tri_en6 = [[[3, 4], [8, 7], [8, 4], [9, 10], [74, 86], [63, 65]]]
+
+# Declare 2 variables tridimensionales 2x3x1, 5x5x2, 3x6x5 y asignar valores flotantes
+
+#Declare 2 variables tridimensionales 2x3x1
+va_tri_flo1 = [[[1.1], [2.1], [3.3]],  [[4.5], [5.5], [6.7]]]
+va_tri_flo2 = [[[2.1], [2.2], [3.2]],  [[4.1], [5.1], [6.1]]]
+
+#Declare 2 variables tridimensionales 5x5x2
+
+va_tri_flo3 = [[[1.1, 1.2], [2.1, 2.2], [3.1, 3.4], [3.4, 4.5], [7.6, 4.3]],  [[4.5, 8.9], [5.5, 98.4], [6.8, 8.9], [8.9, 9.4], [5.5, 6.6]]]
+va_tri_flo4 = [[[1.3, 1.4], [2.2, 2.3], [5.1, 6.4], [7.4, 7.5], [7.6, 8.3]],  [[8.5, 8.8], [7.5, 78.4], [4.8, 8.8], [8.4, 6.4], [8.5, 7.6]]]
+
+#Declare 2 variables tridimensionales 3x6x5.
+va_tri_flo5 = [[[2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5]]]
+va_tri_flo6 = [[[2.5, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5], [2.2, 2.3, 6.7, 7.8, 3.5]]]
+
+# Declare 2 variables tridimensionales 2x3x3, 5x5x4, 3x6x1 y asignar valores texto
+
+#variables tridimensionales 2x3x3
+va_tri_tex1 = [[['uno',  'dos', 'tres']],     [['cuatro'], ['cinco'], ['seis']]]
+va_tri_tex2 = [[['one', 'yes',  'brother']],  [['lobo'],   ['tres'],  ['dos']]]
+
+# print(va_tri_tex1+va_tri_tex2)
+
+# Declare 2 variables tridimensionales  5x5x4
+va_tri_tex3 = [[['Perez', 'Juan', 'Carlos', 'Carmen']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']]]
+va_tri_tex4 = [[['Pedro', 'Juni', 'Cars', 'mesa']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']], [['Felix', 'Rosa', 'Patri', 'Adri']]]
+#print(vatri_tri_tex4)
+
+# variables tridimensionales 3x6x1
+va_tri_tex5 = [[['cero'], ['dos'], ['Uno'], ['trece'], ['doce'], ['Nose']],  [['dos'], ['Uno'], ['carlos'], ['Juan'], ['Washo'], ['trece']],  [['dos'], ['Uno'], ['carlos'], ['Juan'], ['Washo'], ['trece']]]
+va_tri_tex6 = [[['cero'], ['dos'], ['Uno'], ['trece'], ['doce'], ['Nose']],  [['dos'], ['Uno'], ['carlos'], ['Juan'], ['Washo'], ['trece']],  [['dos'], ['Uno'], ['carlos'], ['Juan'], ['Washo'], ['trece']]]
+
+# Declare 2 variables tridimensionales 2x3x2, 5x5x1, 3x6x2 y asignar valores enteros, Flotante, texto
+
+#variables tridimensionales 2x3x2
+va_tri_en_flo_tex1 = [[[1, 2.3], ['tres', 1], ['Uno', 5]],  [[4, 'cero'], [2.4, 6], ['cinco', '2']]]
+va_tri_en_flo_tex2 = [[[1, 2.3], ['tres', 1], ['Uno', 5]],  [[4, 'cero'], [2.4, 6], ['cinco', '2']]]
+
+#variables tridimensionales 5x5x1
+
+va_tri_en_flo_tex3 = [[[1], [3.4], ['dos'], [1], [3.6]],  [[1], [3.4], ['dos'], [1], [3.6]],  [[1], [3.4], ['dos'], [1], [3.6]], [[1], [3.4], ['dos'], [1], [3.6]], [[1], [3.4], ['dos'], [1], [3.6]]]
+
+va_tri_en_flo_tex4 = [[[1], [3.4], ['dos'], [1], [3.6]],  [[1], [3.4], ['dos'], [1], [3.6]],  [[1], [3.4], ['dos'], [1], [3.6]], [[1], [3.4], ['dos'], [1], [3.6]], [[1], [3.4], ['dos'], [1], [3.6]]]
+
+#variables tridimensionales 3x6x2
+
+va_tri_en_flo_tex5 = [[['dos'], [1], [3.4], ['tre'], [2], [2.3]], [['dos'], [1], [3.4], ['tre'], [2], [2.3]], [['dos'], [1], [3.4], ['tre'], [2], [2.3]]]
+va_tri_en_flo_tex6 = [[['dos'], [1], [3.4], ['tre'], [2], [2.3]], [['dos'], [1], [3.4], ['tre'], [2], [2.3]], [['dos'], [1], [3.4], ['tre'], [2], [2.3]]]
+
+
+
+
+
